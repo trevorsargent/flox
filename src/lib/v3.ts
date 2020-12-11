0163a9e427d725c7b2cb7f6ce02f2078613f0ae0
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-12-11 10:38:42 -0800

    org v3 lib

--- src/lib/v3.ts
+++ src/lib/v3.ts
@@ -1,26 +1,34 @@
+// 
+// 
+// Types
+
 export interface V3 {
     x: number
     y: number
     z: number
 }
 
-export const applyToComponents = (cb: (c: number) =>  number) => (a: V3): V3 => ({
-    x: cb(a.x), 
-    y: cb(a.y),
-    z: cb(a.z)
+// 
+// 
+// Utils
+
+export const applyToComponents = (apply: (c: number) =>  number) => (a: V3): V3 => ({
+    x: apply(a.x), 
+    y: apply(a.y),
+    z: apply(a.z)
 })
 
-export const mixComponents = (cb: (aa: number) => (bb: number) => number) => (a: V3) => (b: V3) => ({
-    x: cb(a.x)(b.x), 
-    y: cb(a.y)(b.y),
-    z: cb(a.z)(b.z)
+export const mixComponents = (mix: (aa: number) => (bb: number) => number) => (a: V3) => (b: V3) => ({
+    x: mix(a.x)(b.x), 
+    y: mix(a.y)(b.y),
+    z: mix(a.z)(b.z)
 })
 
-export const add = (a: V3) => (b: V3): V3 => mixComponents(aa => bb => aa + bb)(a)(b)
+// 
+// 
+// Single Vector Operations
 
 export const invert = (a: V3): V3 => applyToComponents(c => -1 * c)(a)
-
-export const sub = (a: V3) => (b: V3): V3 => add(a)(invert(b))
 
 export const magSquared = (a: V3): number => {
     const sq = applyToComponents(c => c * c)(a)   
@@ -36,3 +44,10 @@ export const normalize = (a: V3): V3 => {
 
 export const scale = (scale: number) => (a: V3): V3 => applyToComponents(c => c * scale)(a)
 
+// 
+// 
+// Two Vector Operations
+
+export const add = (a: V3) => (b: V3): V3 => mixComponents(aa => bb => aa + bb)(a)(b)
+
+export const sub = (a: V3) => (b: V3): V3 => add(a)(invert(b))
