87ec84a5967d0a32928cc2b36e59ebb7b96644f7
Trevor Sargent <trevor.a.sargent@gmail.com>
2021-02-28 00:41:02 -0800

    add bevy and start

--- src/flock/types/agent.rs
+++ src/flock/types/agent.rs
@@ -1,55 +1,55 @@
-mod agent_spec;
-use vex::Vex;
+// mod agent_spec;
+// use vex::Vex;
 
-#[derive(Debug)]
-pub struct Agent {
-    pos: Vex,
-    vel: Vex,
-    acc: Vex,
-}
+// #[derive(Debug)]
+// pub struct Agent {
+//     pos: Vex,
+//     vel: Vex,
+//     acc: Vex,
+// }
 
-pub trait Movement {
-    fn advance(&mut self);
-}
+// pub trait Movement {
+//     fn advance(&mut self);
+// }
 
-pub trait Force {
-    fn apply_force(&mut self, force: Vex);
-}
-pub struct AgentParams {
-    pub pos: Option<Vex>, 
-    pub vel: Option<Vex>,
-    pub acc: Option<Vex>
-}
+// pub trait Force {
+//     fn apply_force(&mut self, force: Vex);
+// }
+// pub struct AgentParams {
+//     pub pos: Option<Vex>,
+//     pub vel: Option<Vex>,
+//     pub acc: Option<Vex>
+// }
 
-impl Agent {
-    #[allow(dead_code)]
-    pub fn new(params: AgentParams) -> Agent {
-        Self {
-            pos: match params.pos {
-                Some(p) => p,
-                None => Vex::new(),
-            },
-            vel: match params.vel {
-                Some(v) => v,
-                None => Vex::new(),
-            },
-            acc: match params.acc {
-                Some(a) => a,
-                None => Vex::new(),
-            },
-        }
-    }
-}
+// impl Agent {
+//     #[allow(dead_code)]
+//     pub fn new(params: AgentParams) -> Agent {
+//         Self {
+//             pos: match params.pos {
+//                 Some(p) => p,
+//                 None => Vex::new(),
+//             },
+//             vel: match params.vel {
+//                 Some(v) => v,
+//                 None => Vex::new(),
+//             },
+//             acc: match params.acc {
+//                 Some(a) => a,
+//                 None => Vex::new(),
+//             },
+//         }
+//     }
+// }
 
-impl Movement for Agent {
-    fn advance(&mut self) {
-        self.vel.assign(self.vel + self.acc);
-        self.pos.assign(self.pos + self.vel);
-    }
-}
+// impl Movement for Agent {
+//     fn advance(&mut self) {
+//         self.vel.assign(self.vel + self.acc);
+//         self.pos.assign(self.pos + self.vel);
+//     }
+// }
 
-impl Force for Agent {
-    fn apply_force(&mut self, force: Vex){
-        self.vel.assign(self.vel + force)
-    }
-}
+// impl Force for Agent {
+//     fn apply_force(&mut self, force: Vex){
+//         self.vel.assign(self.vel + force)
+//     }
+// }
--- src/flock/types/flock.rs
+++ src/flock/types/flock.rs
@@ -1,35 +1,33 @@
-use crate::agent::{Agent, AgentParams};
-mod flock_spec;
+// use crate::agent::{Agent, AgentParams};
+// mod flock_spec;
 
-#[derive(Default, Clone)]
-struct FlockParams {
-    population: Option<i32>
-}
+// #[derive(Default, Clone)]
+// struct FlockParams {
+//     population: Option<i32>
+// }
 
-struct Flock {
-    #[allow(dead_code)]
-    members: Vec<Agent>
-}
+// struct Flock {
+//     #[allow(dead_code)]
+//     members: Vec<Agent>
+// }
 
-impl Flock {
-    #[allow(dead_code)]
-    pub fn new(params: FlockParams) -> Self {
+// impl Flock {
+//     #[allow(dead_code)]
+//     pub fn new(params: FlockParams) -> Self {
 
-        let mut members = Vec::new();
+//         let mut members = Vec::new();
 
-        match params.population {
-            Some(pop) => {
-                    for _ in 0..pop {
-                        members.push(Agent::new(AgentParams {pos: None, vel: None, acc: None}))
-                    }
-            },
-            None => {},
-        }
+//         match params.population {
+//             Some(pop) => {
+//                     for _ in 0..pop {
+//                         members.push(Agent::new(AgentParams {pos: None, vel: None, acc: None}))
+//                     }
+//             },
+//             None => {},
+//         }
 
-        Self {
-            members,
-        }
-    }
-}
-
-
+//         Self {
+//             members,
+//         }
+//     }
+// }
